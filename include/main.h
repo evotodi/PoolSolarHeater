63e8fbeb2b82169e3dde6cfaccfc35281c6aa5ce
evotodi <evotodi@gmail.com>
2023-06-14 18:47:46 -0400

    Moving to new 18x33 pool

--- include/main.h
+++ include/main.h
@@ -15,14 +15,23 @@
 #include <WiFiClient.h>
 #include <ESPAsyncWebServer.h>
 #include <Oversampling.h>
+#include <SolarCalculator.h>
 
 #define DEBUG 1
 //#define TESTING 1
-#define LOG_TO_TELNET 1
+// #define LOG_TO_TELNET 1
 //#define NO_ENV_HOUR_CHECK 1
 //#define NO_ENV_SP_CHECK 1
 
+#define LOOP_DAT_DLY      5*1E3
+#define LOOP_PROC_DLY     5*1E3
+#define LOOP_PUB_DLY      15*1E3
+#define LOOP_SLEEP_DLY   60*1E3
+
 #define boolToStr(x) ((x)?"Yes":"No")
+
+#define LATITUDE 38.502539
+#define LONGITUDE -86.163691
 
 #define TELNET_PORT 23
 #define DS_TEMP_PRECISION 12
@@ -36,44 +45,21 @@
 
 #define ADC_LIGHT      0
 #define ADC_AMBIANT    1
-#define ADC_FRAME1     2
-#define ADC_FRAME2     3
 
 #define DARK_DEFAULT      400
-#define GPM_DEFAULT       5.0
 #define SP_DEFAULT        95.0
 #define TOFS_DEFAULT      0.0
-#define AIR_DIFF_DEFAULT  5
-#define WATTS_MIN_DEFAULT 200.0
 
-#define LOOP_DAT_DLY      5*1E3
-#define LOOP_PROC_DLY     5*1E3
-#define LOOP_PUB_DLY      15*1E3
-
-#define LOOP_SLEEP_DLY   60*1E3
-
-#define TIME_OFFSET_DST  -14400
-#define TIME_OFFSET_ST  -18000
-#define DST_BEGIN_DAY    13
-#define DST_BEGIN_MONTH  3
-#define DST_END_DAY      6
-#define DST_END_MONTH    11
 #define STAY_DARK_CNT    3
 #define DAY_START_HOUR   9  // GMT - TIME_OFFSET
 #define DAY_END_HOUR     20  // GMT - TIME_OFFSET
-
-#define SP_HYSTERESIS 2.0 // Degrees under setpoint before turning pump on
-#define CIRC_LOOP_OFF_CNT 59 // Min of 2; (This * LOOP_CIRC_DLY) + LOOP_CIRC_DLY; 3 * 10 + 10 = 40 seconds
-#define CIRC_LOOP_ON_CNT 4 //Min of 2; (This * LOOP_CIRC_DLY) - LOOP_CIRC_DLY; 2 * 10 - 10 = 10 seconds
 
 //>> Structures
 struct PSHConfig
 {
     int16_t dark;
-    float gpm;
     float setpoint;
     int airDiff;
-    float minWatts;
 };
 
 struct DTSetting
@@ -81,19 +67,19 @@ struct DTSetting
     char addr[18];
     float offset;
 };
+
+struct Solar
+{
+    double azimuth;
+    double elevation;
+};
 //<< Structures
 
 //>> Function Prototypes
-time_t getNtpTime();
-int getTimeOffset(time_t t);
-const char* getTimestamp();
 void setupHandler();
 void loopHandler();
-#ifdef LOG_TO_TELNET
-void handleTelnet();
-void printHelp();
-void onHomieEvent(const HomieEvent& event);
-#endif
+time_t getNtpTime();
+const char* getTimestamp();
 void strToAddress(const String& addr, DeviceAddress deviceAddress);
 void printAddress(DeviceAddress deviceAddress);
 void setupOwSensors();
@@ -102,14 +88,11 @@ int16_t adcReadCallback(uint8_t channel);
 void parseNTCSettings(const char * settings, const char * name, ThermistorSettings * ts);
 PSHConfig parsePSHSettings(const char * settings, const char * name);
 DTSetting parseDTSettings(const char * settings, const char * name);
-float calcWatts(float tempIn, float tempOut);
-bool envAllowPump();
-void doProcess();
-bool turnPumpOn();
-bool turnPumpOff();
-void toggleOverrideEnv();
-bool pumpOnHandler(const HomieRange& range, const String& value);
-#ifdef TESTING
-void testing();
+#ifdef LOG_TO_TELNET
+void handleTelnet();
+void printHelp();
+void onHomieEvent(const HomieEvent& event);
 #endif
+void toggleOverrideEnv();
+Solar getSolar();
 //<< Function Prototypes
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ DeviceAddress tempSensorIn; // = { 0x28, 0x9D, 0x6F, 0x49, 0xF6, 0x7A, 0x3C, 0xF
 DeviceAddress tempSensorOut; // = { 0x28, 0x3E, 0x47, 0x49, 0xF6, 0xC1, 0x3C, 0x1A };
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+NTPClient timeClient(ntpUDP, "192.168.14.254", 0);
 
 #ifdef LOG_TO_TELNET
 WiFiServer TelnetServer(TELNET_PORT);
@@ -21,21 +21,19 @@ MCP3204 mcp(MCP_DIN, MCP_DOUT, MCP_CLK);
 char timestampStrBuf[1024];
 EasyStringStream timestampStream(timestampStrBuf, 1024);
 
-PSHConfig pshConfigs = {int16_t(DARK_DEFAULT), float(GPM_DEFAULT), float(SP_DEFAULT)};
+PSHConfig pshConfigs = {int16_t(DARK_DEFAULT), float(SP_DEFAULT)};
 DTSetting tinSettings = {"0000000000000000", float(TOFS_DEFAULT)};
 DTSetting toutSettings = {"0000000000000000", float(TOFS_DEFAULT)};
 
-ThermistorSettings frame1Settings = {5.0, 5.0, 10000, 10000, 25, 4096, 3950, 5, 20};
-ThermistorSettings frame2Settings = {5.0, 5.0, 10000, 10000, 25, 4096, 3950, 5, 20};
 ThermistorSettings ambiantSettings = {5.0, 5.0, 100000, 100000, 25, 4096, 3950, 5, 20};
-ThermistorSettings poolSettings = {3.3, 3.3, 100000, 100000, 25, 4096, 3950, 5, 20};
-
-Thermistor ntcFrame1(mcpReadCallback, frame1Settings);
-Thermistor ntcFrame2(mcpReadCallback, frame2Settings);
 Thermistor ntcAmbiant(mcpReadCallback, ambiantSettings);
-Thermistor ntcPool(adcReadCallback, poolSettings);
 
 Oversampling adc(10, 12, 2);
+
+HomieSetting<const char *> configSetting("config", "config kv");
+HomieSetting<const char *> ambiantSetting("ambiant", "ambiant kv");
+HomieSetting<const char *> tinSetting("tin", "tin json");
+HomieSetting<const char *> toutSetting("tout", "tout json");
 
 unsigned long currentMillis = 0;
 unsigned long intervalData = LOOP_DAT_DLY;
@@ -49,68 +47,17 @@ unsigned long previousMillisHB = 0;
 
 bool isDark = false;
 bool realDark = false;
-bool pumpOn = false;
 bool atSetpoint = false;
 bool overrideEnv = false;
-
-int16_t light = 0;
 int telnetBuffer;
+int16_t light = 0;
 char stayDarkCnt = 0;
-int timeOffset = 0;
-
 float tin = -127.0;
 float tout = -127.0;
-float f1 = -127.0;
-float f2 = -127.0;
 float air = -127.0;
-float pool = -127.0;
-float wattsT = 0.0;
-float wattsF1 = 0.0;
-float wattsF2 = 0.0;
 
-HomieNode infoNode("info", "Info", "string");
-HomieNode tempNode("temp", "Temps", "string");
-HomieNode lightNode("light", "Light", "string");
-
-HomieSetting<const char *> configSetting("config", "config kv");
-HomieSetting<const char *> frame1Setting("frame1", "frame 1 kv");
-HomieSetting<const char *> frame2Setting("frame2", "frame 2 kv");
-HomieSetting<const char *> ambiantSetting("ambiant", "ambiant kv");
-HomieSetting<const char *> poolSetting("pool", "pool kv");
-HomieSetting<const char *> tinSetting("tin", "tin json");
-HomieSetting<const char *> toutSetting("tout", "tout json");
-
-#ifdef TESTING
-void testing()
+void setup()
 {
-    delay(200);
-    Serial.println("Testing!!!");
-
-    DTSetting fs{};
-    char a[18];
-    float b;
-
-//    sscanf("addr=283E4749F6C13C1A\t;offset=0.0", "addr=%[^\t];offset=%f", // NOLINT(cert-err34-c)
-//           &a, &b
-//    );
-
-    sscanf("addr=283E4749F6C13C1A;offset=0.1", "addr=%[0-9a-fA-F];offset=%f",
-           &a, &b
-    );
-
-    Serial.print("Address = "); Serial.println(a);
-    Serial.print("Offset = "); Serial.println(b);
-
-    while (true){
-        delay(60000);
-    }
-}
-#endif
-
-void setup() {
-    pinMode(LED_BUILTIN_AUX, OUTPUT);
-    pinMode(RLY_PIN, OUTPUT);
-
 #ifdef DEBUG
     Serial.begin(115200);
 #else
@@ -118,10 +65,6 @@ void setup() {
 #endif
 
     WiFi.mode(WIFI_STA);
-
-#ifdef TESTING
-    testing();
-#endif
 
     mcp.begin(MCP_CS);
 #ifdef DEBUG
@@ -134,27 +77,9 @@ void setup() {
     Serial.println(mcp.maxValue());
 #endif
 
-    Homie_setFirmware("bare-minimum", "1.0.0");
+    Homie_setFirmware("bare-minimum", "1.0.0")
     Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
-    Homie_setBrand("PoolHeater");
-
-    tempNode.advertise("tin").setName("TempIn").setDatatype("float").setUnit("F");
-    tempNode.advertise("tout").setName("TempOut").setDatatype("float").setUnit("F");
-    tempNode.advertise("f1").setName("Frame1").setDatatype("float").setUnit("F");
-    tempNode.advertise("f2").setName("Frame2").setDatatype("float").setUnit("F");
-    tempNode.advertise("air").setName("Air").setDatatype("float").setUnit("F");
-    tempNode.advertise("pool").setName("Pool").setDatatype("float").setUnit("F");
-
-    lightNode.advertise("light").setName("LightLvl").setDatatype("float").setUnit("");
-    lightNode.advertise("dark").setName("IsDark").setDatatype("string");
-    lightNode.advertise("real_dark").setName("RealDark").setDatatype("string");
-
-    infoNode.advertise("timestamp").setName("Timestamp").setDatatype("string").setUnit("");
-    infoNode.advertise("watts_t").setName("Watts Total").setDatatype("float").setUnit("W");
-    infoNode.advertise("watts_f1").setName("Watts F1").setDatatype("float").setUnit("W");
-    infoNode.advertise("watts_f2").setName("Watts F2").setDatatype("float").setUnit("W");
-    infoNode.advertise("pump").setName("PumpOn").setDatatype("boolean").settable(pumpOnHandler);
-    infoNode.advertise("at_setpoint").setName("At Setpoint").setDatatype("string");
+    Homie_setBrand("PoolHeater")
 
 #ifdef LOG_TO_TELNET
     Homie.setLoggingPrinter(&Telnet);
@@ -164,16 +89,17 @@ void setup() {
     Homie.setup();
 }
 
-void loop() {
+void loop() 
+{
     Homie.loop();
 #ifdef LOG_TO_TELNET
     handleTelnet();
 #endif
 }
 
-void setupHandler() {
+void setupHandler() 
+{
     // Code which should run AFTER the Wi-Fi is connected.
-
 #ifdef LOG_TO_TELNET
     TelnetServer.begin();
     Serial.print("Starting telnet server on port "); Serial.println(TELNET_PORT);
@@ -183,49 +109,18 @@ void setupHandler() {
     timeClient.forceUpdate();
     yield();
 
-    if(timeClient.isTimeSet()){
-        timeOffset = getTimeOffset(now());
-        yield();
-        Homie.getLogger() << "Time offset = " << timeOffset << endl;
-        timeClient = NTPClient(ntpUDP, long(timeOffset));
-        timeClient.begin();
-        timeClient.forceUpdate();
-        yield();
-    }
-
     setSyncProvider(getNtpTime);
     setSyncInterval(600);
     yield();
 
     pshConfigs = parsePSHSettings(configSetting.get(), "Config");
 
-    parseNTCSettings(frame1Setting.get(), "Frame 1", &frame1Settings);
-    parseNTCSettings(frame2Setting.get(), "Frame 2", &frame2Settings);
     parseNTCSettings(ambiantSetting.get(), "Ambiant", &ambiantSettings);
-    parseNTCSettings(poolSetting.get(), "Pool", &poolSettings);
 
     tinSettings = parseDTSettings(tinSetting.get(), "Tin");
     strToAddress(tinSettings.addr, tempSensorIn);
     toutSettings = parseDTSettings(toutSetting.get(), "Tout");
     strToAddress(toutSettings.addr, tempSensorOut);
-    yield();
-
-    ntcFrame1.setSeriesResistor(&frame1Settings.seriesResistor);
-    ntcFrame1.setThermistorNominal(&frame1Settings.thermistorNominal);
-    ntcFrame1.setBCoef(&frame1Settings.bCoef);
-    ntcFrame1.setTemperatureNominal(&frame1Settings.temperatureNominal);
-    ntcFrame1.setVcc(&frame1Settings.vcc);
-    ntcFrame1.setAnalogReference(&frame1Settings.analogReference);
-    Homie.getLogger() << "Frame 1 settings = " << ntcFrame1.dumpSettings() << endl;
-    yield();
-
-    ntcFrame2.setSeriesResistor(&frame2Settings.seriesResistor);
-    ntcFrame2.setThermistorNominal(&frame2Settings.thermistorNominal);
-    ntcFrame2.setBCoef(&frame2Settings.bCoef);
-    ntcFrame2.setTemperatureNominal(&frame2Settings.temperatureNominal);
-    ntcFrame2.setVcc(&frame2Settings.vcc);
-    ntcFrame2.setAnalogReference(&frame2Settings.analogReference);
-    Homie.getLogger() << "Frame 2 settings = " << ntcFrame2.dumpSettings() << endl;
     yield();
 
     ntcAmbiant.setSeriesResistor(&ambiantSettings.seriesResistor);
@@ -237,19 +132,11 @@ void setupHandler() {
     Homie.getLogger() << "Ambiant settings = " << ntcAmbiant.dumpSettings() << endl;
     yield();
 
-    ntcPool.setSeriesResistor(&poolSettings.seriesResistor);
-    ntcPool.setThermistorNominal(&poolSettings.thermistorNominal);
-    ntcPool.setBCoef(&poolSettings.bCoef);
-    ntcPool.setTemperatureNominal(&poolSettings.temperatureNominal);
-    ntcPool.setVcc(&poolSettings.vcc);
-    ntcPool.setAnalogReference(&poolSettings.analogReference);
-    Homie.getLogger() << "Pool settings = " << ntcPool.dumpSettings() << endl;
-    yield();
-
     setupOwSensors();
 }
 
-void loopHandler() {
+void loopHandler() 
+{
     currentMillis = millis();
 
     // Gather data
@@ -291,39 +178,12 @@ void loopHandler() {
         yield();
         tout = sensors.getTempF(tempSensorOut) + toutSettings.offset;
         yield();
-        if(pumpOn) {
-            wattsT = calcWatts(tin, tout);
-        }else{
-            wattsT = float(0);
-        }
-        Homie.getLogger() << "Tin = " << tin << " °F " << "Tout = " << tout << " °F " << " Watts = " << wattsT << endl;
+
+        Homie.getLogger() << "Tin = " << tin << " °F " << "Tout = " << tout << " °F " << endl;
         yield();
 
-
-        pool = float(ntcPool.readTempF(A0));
-        yield();
         air = float(ntcAmbiant.readTempF(ADC_AMBIANT));
-        Homie.getLogger() << "Pool = " << pool << " °F  " << "Air = " << air << " °F" << endl;
-        yield();
-
-        f1 = float(ntcFrame1.readTempF(ADC_FRAME1));
-        yield();
-        if(pumpOn) {
-            wattsF1 = calcWatts(air, f1);
-        }else{
-            wattsF1 = float(0);
-        }
-        Homie.getLogger() << "Frame 1 = " << f1 << " °F " << " Watts = " << wattsF1 << endl;
-        yield();
-
-        f2 = float(ntcFrame2.readTempF(ADC_FRAME2));
-        yield();
-        if(pumpOn) {
-            wattsF2 = calcWatts(air, f2);
-        }else{
-            wattsF2 = float(0);
-        }
-        Homie.getLogger() << "Frame 2 = " << f2 << " °F " << " Watts = " << wattsF2 << endl;
+        Homie.getLogger() << "Air = " << air << " °F" << endl;
         yield();
 
         light = mcp.analogRead(ADC_LIGHT);
@@ -335,7 +195,6 @@ void loopHandler() {
         Homie.getLogger() << "Light level = " << light << " Dark out = " << boolToStr(isDark) << " Real Dark = " << boolToStr(realDark) << endl;
         yield();
 
-        Homie.getLogger() << "Pump On = " << boolToStr(pumpOn) << endl;
         Homie.getLogger() << "At setpoint = " << boolToStr(atSetpoint) << endl;
         yield();
 
@@ -348,38 +207,10 @@ void loopHandler() {
     if (currentMillis - previousMillisProc > intervalProc || previousMillisProc == 0) {
         Homie.getLogger() << "PROCESS:" << endl;
 
-        doProcess();
+//        doProcess();
 
         Homie.getLogger() << endl;
         previousMillisProc = currentMillis;
-    }
-    yield();
-
-    // Publish data
-    if (currentMillis - previousMillisPub > intervalPub || previousMillisPub == 0) {
-        Homie.getLogger() << "PUBLISH:" << endl;
-
-        infoNode.setProperty("timestamp").send(getTimestamp());
-        infoNode.setProperty("watts_t").send(String(wattsT));
-        infoNode.setProperty("watts_f1").send(String(wattsF1));
-        infoNode.setProperty("watts_f2").send(String(wattsF2));
-        infoNode.setProperty("pump").send(pumpOn ? "true" : "false");
-        infoNode.setProperty("at_setpoint").send(atSetpoint ? "Yes" : "No");
-
-        lightNode.setProperty("light").send(String(light));
-        lightNode.setProperty("dark").send(isDark ? "Yes" : "No");
-        lightNode.setProperty("real_dark").send(realDark ? "Yes" : "No");
-
-        tempNode.setProperty("tin").send(String(tin));
-        tempNode.setProperty("tout").send(String(tout));
-        tempNode.setProperty("f1").send(String(f1));
-        tempNode.setProperty("f2").send(String(f2));
-        tempNode.setProperty("air").send(String(air));
-        tempNode.setProperty("pool").send(String(pool));
-
-        Homie.getLogger() << "Published data" << endl;
-        Homie.getLogger() << endl;
-        previousMillisPub = currentMillis;
     }
     yield();
 
@@ -388,23 +219,6 @@ void loopHandler() {
         digitalWrite(LED_BUILTIN_AUX, !digitalRead(LED_BUILTIN_AUX));
         previousMillisHB = currentMillis;
     }
-
-}
-
-void doProcess()
-{
-    if(tin >= pshConfigs.setpoint) {
-        Homie.getLogger() << "Set point reached" << endl;
-        turnPumpOff();
-        atSetpoint = true;
-        return;
-    }
-
-    if(tin < (pshConfigs.setpoint - float(SP_HYSTERESIS))){
-        atSetpoint = false;
-    }
-
-    turnPumpOn();
 }
 
 #ifdef LOG_TO_TELNET
@@ -467,34 +281,15 @@ void onHomieEvent(const HomieEvent& event)
 }
 #endif
 
-time_t getNtpTime() {
+time_t getNtpTime() 
+{
     timeClient.forceUpdate();
-    Homie.getLogger() << "NTP Updated. Current time is " << timeClient.getEpochTime() << endl << endl;
+    Homie.getLogger() << "NTP Updated. Current time is [ Unix: " << timeClient.getEpochTime() << " Human: " << timeClient.getFormattedTime() << " ]" << endl;
     return time_t(timeClient.getEpochTime());
 }
 
-int getTimeOffset(time_t t)
+const char *getTimestamp() 
 {
-    if(month(t) == DST_BEGIN_MONTH && day(t) >= DST_BEGIN_DAY){
-        return TIME_OFFSET_DST;
-    }
-
-    if(month(t) == DST_END_MONTH && day(t) <= DST_END_DAY){
-        return TIME_OFFSET_ST;
-    }
-
-    if(month(t) > DST_BEGIN_MONTH && month(t) < DST_END_MONTH){
-        return TIME_OFFSET_DST;
-    }
-
-    if(month(t) > DST_END_MONTH){
-        return TIME_OFFSET_ST;
-    }
-
-    return TIME_OFFSET_DST;
-}
-
-const char *getTimestamp() {
     timestampStream.reset();
     timestampStream << String(year()) << "-";
 
@@ -541,7 +336,8 @@ const char *getTimestamp() {
     return timestampStream.get();
 }
 
-void strToAddress(const String &addr, DeviceAddress deviceAddress) {
+void strToAddress(const String &addr, DeviceAddress deviceAddress) 
+{
     char byt[3] = {0, 0, 0};
     unsigned int number;
 
@@ -555,14 +351,16 @@ void strToAddress(const String &addr, DeviceAddress deviceAddress) {
 
 }
 
-void printAddress(DeviceAddress deviceAddress) {
+void printAddress(DeviceAddress deviceAddress) 
+{
     for (uint8_t i = 0; i < 8; i++) {
         if (deviceAddress[i] < 16) Serial.print("0");
         Serial.print(deviceAddress[i], HEX);
     }
 }
 
-void setupOwSensors() {
+void setupOwSensors() 
+{
 #ifdef DEBUG
     Serial.print("Locating devices...");
 #endif
@@ -628,11 +426,13 @@ void setupOwSensors() {
 #endif
 }
 
-int16_t mcpReadCallback(uint8_t channel) {
+int16_t mcpReadCallback(uint8_t channel) 
+{
     return mcp.analogRead(channel);
 }
 
-int16_t adcReadCallback(uint8_t channel) {
+int16_t adcReadCallback(uint8_t channel) 
+{
     return int16_t(adc.read(channel));
 }
 
@@ -653,15 +453,13 @@ PSHConfig parsePSHSettings(const char * settings, const char * name)
 {
     PSHConfig fs{};
 
-    sscanf(settings, "dark=%hi;gpm=%f;setpoint=%f;minWatts=%f;airDiff=%i", // NOLINT(cert-err34-c)
-           &fs.dark, &fs.gpm, &fs.setpoint, &fs.minWatts, &fs.airDiff
+    sscanf(settings, "dark=%hi;setpoint=%f;airDiff=%i", // NOLINT(cert-err34-c)
+           &fs.dark, &fs.setpoint, &fs.airDiff
     );
 
     Homie.getLogger() << "Parsed " << name << " settings = >>>" << settings << "<<<" << endl;
     Homie.getLogger() << "Dark = " << fs.dark << endl;
-    Homie.getLogger() << "GPM = " << fs.gpm << endl;
     Homie.getLogger() << "Setpoint = " << fs.setpoint << endl;
-    Homie.getLogger() << "Min Watts = " << fs.minWatts << endl;
     Homie.getLogger() << "Min Air->Frame Diff = " << fs.airDiff << endl;
     Homie.getLogger() << endl;
 
@@ -674,9 +472,7 @@ DTSetting parseDTSettings(const char * settings, const char * name)
 {
     DTSetting fs{};
 
-    sscanf(settings, "addr=%[0-9a-fA-F];offset=%f", // NOLINT(cert-err34-c)
-           &fs.addr, &fs.offset
-    );
+    sscanf(settings, "addr=%[0-9a-fA-F];offset=%f", &fs.addr, &fs.offset ); // NOLINT(cert-err34-c)
 
     Homie.getLogger() << "Parsed " << name << " settings = >>>" << settings << "<<<" << endl;
     Homie.getLogger() << "Address = " << fs.addr << endl;
@@ -687,94 +483,29 @@ DTSetting parseDTSettings(const char * settings, const char * name)
 }
 #pragma clang diagnostic pop
 
-float calcWatts(float tempIn, float tempOut) {
-    const float dt = tempOut - tempIn;
-    const auto c = float(0.00682);
-    float rtn;
-    rtn = (pshConfigs.gpm * dt) / c;
-    return rtn;
-}
-
-bool envAllowPump()
-{
-    if(overrideEnv){
-        return true;
-    }
-#ifndef NO_ENV_HOUR_CHECK
-    if(hour() < DAY_START_HOUR || hour() >= DAY_END_HOUR) {
-        Homie.getLogger() << "Env: Now hour " << hour() << " not between " << DAY_START_HOUR << " and " << DAY_END_HOUR << endl;
-        return false;
-    }
-#endif
-
-    if(realDark){
-        Homie.getLogger() << "Env: Real dark" << endl;
-        return false;
-    }
-
-#ifndef NO_ENV_SP_CHECK
-    if (air < pshConfigs.setpoint) {
-        if (((int(f1) + int(f2)) / 2) < (int(air) + pshConfigs.airDiff)) {
-            Homie.getLogger() << "Env: Frame to air not enough diff" << endl;
-            return false;
-        }
-    }
-#endif
-
-    if( ((int(f1) + int(f2)) / 2) <= int(pool)) {
-        Homie.getLogger() << "Env: Frame less than pool temp" << endl;
-        return false;
-    }
-
-    return true;
-}
-
-bool turnPumpOn()
-{
-    if(!envAllowPump()){
-        Homie.getLogger() << "Env not allow pump on" << endl;
-        turnPumpOff();
-        return false;
-    }
-
-    if(!pumpOn){
-        Homie.getLogger() << "Pump turned on" << endl;
-    }
-    digitalWrite(RLY_PIN, HIGH);
-    pumpOn = true;
-    return true;
-}
-
-bool turnPumpOff()
-{
-    if(pumpOn){
-        Homie.getLogger() << "Pump turned off" << endl;
-    }
-    digitalWrite(RLY_PIN, LOW);
-    pumpOn = false;
-
-    return true;
-}
-
 void toggleOverrideEnv()
 {
     overrideEnv = !overrideEnv;
 }
 
-bool pumpOnHandler(const HomieRange& range, const String& value) {
-    if (value != "true" && value != "false") return false;
-    bool on = (value == "true");
-    if(on){
-        overrideEnv = true;
-        turnPumpOn();
-    }else{
-        overrideEnv = false;
-        turnPumpOff();
-    }
-    infoNode.setProperty("pump").send(value);
-    Homie.getLogger() << "Pump is forced " << (on ? "on" : "off") << endl;
+Solar getSolar()
+{
+    time_t utc = now();
+    Solar solar{};
 
-    return true;
+    // Calculate the solar position, in degrees
+    calcHorizontalCoordinates(utc, LATITUDE, LONGITUDE, solar.azimuth, solar.elevation);
+
+    // // Print results
+#ifdef DEBUG  
+    Serial.print("Solar Time: ");
+    Serial.println(utc);
+    Serial.print(F("Solar Az: "));
+    Serial.print(solar.azimuth);
+    Serial.print(F("°  El: "));
+    Serial.print(solar.elevation);
+    Serial.println(F("°"));
+#endif
+
+    return solar;
 }
-
-
